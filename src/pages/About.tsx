574d421b4e14db9dd0159d5e588b7eb1de8c2c75
Vincent <vincent@Dreemmobil.fritz.box>
2026-05-10 23:45:49 +0200

    online test

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -3,8 +3,7 @@ export default function About() {
   return (
     <div className="mt-10 ml-[20vw]">
       <p className="text-sm text-[#feb42c] blur-[0.3px] transition-all duration-150 [text-shadow:0px_0px_1px_rgba(255,0,0,0.6),0px_0px_2px_rgba(255,0,0,0.8)]">
-        pianist, builder of synth sounds, musical director, producer, arranger +
-        composer, <br /> currently based in Essen, Germany. °
+        badabing badaboom. 
       </p>
     </div>
   );
